46266937f0fd68037948f62d2c62ae0aae98c34c
Eugenio <eugeniodg38@gamil.com>
2024-03-26 12:43:53 +0100

    Evoluzione

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,6 +1,6 @@
 package com.mycompany.app;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -15,6 +15,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        assertFalse( false );
     }
 }
